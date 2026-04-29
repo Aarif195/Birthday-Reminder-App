b696bb7fb640e8946aa8eaf45394115b165ddf1b
Aarif <adebayoabdulazeez195@gmail.com>
2026-04-29 11:41:05 +0100

    add user model

--- index.ts
+++ index.ts
@@ -5,11 +5,15 @@ import { connectDB } from "./src/config/db";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 connectDB();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Birthday Reminder App Backend" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
