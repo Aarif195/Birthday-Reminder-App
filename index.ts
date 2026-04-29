7a57b32441d5c5dce2e6d1a2a2e928c312da34fd
Aarif <adebayoabdulazeez195@gmail.com>
2026-04-29 14:38:01 +0100

    update cors configuration and allowed origins

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./src/config/db";
+import { initCronJobs } from "./src/config/cron";
 import userRoutes from "./src/routes/userRoutes";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -9,14 +11,23 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL || "http://localhost:5173",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true
+    })
+);
+
 connectDB();
+initCronJobs();
 
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Birthday Reminder App Backend" });
+    res.json({ message: "Birthday Reminder App Backend" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
